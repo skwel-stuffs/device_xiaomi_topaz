ae4f6ebd60cd02e407c417553b0d520f44458d92
skwel <abalverde1694@gmail.com>
2025-04-10 12:07:28 +0800

    topaz: correct boot-image-profile.txt path
    error: prebuilts/runtime/mainline/i18n/sdk/Android.bp:9:1: module prebuilt_i18n-bootclasspath-fragment variant android_common: source path frameworks/base/config/boot-image-profile.txt does not exist

--- device.mk
+++ device.mk
@@ -524,7 +524,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi
 
 # Inherit several Android Go configurations
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 
 # Telephony
